280312b35596fd093c2ba51c043a8ebb74f23b31
mabarrionuevo <49962155+mabarrionuevo@users.noreply.github.com>
2019-05-26 15:15:30 -0300

    Add files via upload

---
+++ input/caa.R
@@ -0,0 +1,6 @@
+caa <- paste0(url,"359.3_ACERO_CRUDUDO__11,",
+                  "359.3_HIERRO_PRITAL__21,",
+                  "359.3_LAMINADOS_NTE__28,",
+                  "359.3_LAMINADOS_NTE__31,",
+                  "359.3_LAMINADOS_RIO__24",
+                  csv) %>% read.csv()
---
+++ input/faena.R
@@ -0,0 +1,3 @@
+faena <- paste0(url,"41.1_FA_0_M_10,",
+                    "41.1_FCV_0_M_18",
+                    csv) %>% read.csv()
