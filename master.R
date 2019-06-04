ee43a31ce10c128b129b163b381af29954a27be7
mabarrionuevo <49962155+mabarrionuevo@users.noreply.github.com>
2019-06-04 18:09:45 -0300

    Add files via upload

--- master.R
+++ master.R
@@ -1,6 +1,7 @@
+rm(list=ls())
 source("code/library.R")
 url <- "http://apis.datos.gob.ar/series/api/series/?ids="
-csv <- "&limit=1000&format=csv"
+csv <- "&start_date=2004-01&limit=1000&format=csv"
 
 source("input/emi.R")
 source("input/emae.R")
